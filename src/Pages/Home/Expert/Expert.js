530a363ea95161ddfebd47fdd24ceaaffdbffa95
kabir <makkobir79@gmail.com>
2022-06-22 21:06:44 +0600

    .env file added

--- src/Pages/Home/Expert/Expert.js
+++ src/Pages/Home/Expert/Expert.js
@@ -1,20 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Expert = ({ expert }) => {
   const { name, img } = expert;
   return (
     <div className="g-5 col-sm-12 col-md-6 col-lg-4">
       <div className="card">
-        <img src={img} className="card-img-top" alt="..." />
+        <img src={img} className="card-img-top" alt="expert" />
         <div className="card-body">
           <h5 className="card-title">{name}</h5>
           <p className="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card's content.
           </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
-          </a>
+          <Link to="/" className="btn btn-outline-primary">
+            HIRE ME
+          </Link>
         </div>
       </div>
     </div>
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBrze9SsbqdFmYvRucq8LbpMWPV1lBe7Rw",
-  authDomain: "genius-car-88c43.firebaseapp.com",
-  projectId: "genius-car-88c43",
-  storageBucket: "genius-car-88c43.appspot.com",
-  messagingSenderId: "934576480488",
-  appId: "1:934576480488:web:d854d0d0b418e6f5e7e95d",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
 };
 
 // Initialize Firebase
